65250f5d4e959489ee64952bbd65bf4bc4ff3cec
Kato <zzeroyzz@gmail.com>
2025-06-09 14:29:30 -0400

    Refactors components and updates constants
    Comments out loading logic in App component to potentially improve performance and user experience.
    
    Enhances the consistency and clarity of narrative in About constants by restructuring descriptions and adding meaningful titles.
    
    Removes unnecessary console logs and refines prop types for better maintainability and accuracy.
    
    Revises menu height and updates the resume link for consistency and correctness.
    
    Refactors services section to focus on about me details, aligning with updated display logic.
    
    Adds window management capabilities using hooks to improve UI interaction.
    
    Streamlines UI component handling by removing redundant props and updating image references.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Loading from './components/Loading/Loading';
 import Home from './components/Home/Home';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => setIsLoading(false), 4500);
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setIsLoading(false), 4500);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
 
   return (
     <div className="App">
