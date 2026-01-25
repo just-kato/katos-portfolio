de9d77f9ee14955888e9f3792383d8a8fb77e5b0
Kato <zzeroyzz@gmail.com>
2026-01-24 20:25:10 -0500

    feat: updated portfolio

--- src/components/Portfolio/constants.js
+++ src/components/Portfolio/constants.js
@@ -1,21 +1,23 @@
-import toastedsesame from '../../assets/toastedsesame.png'
-import cntlrfx from '../../assets/cntlrfx.png'
+import TST from "../../assets/TST.png";
+import cntlrfx from "../../assets/cntlrfx.png";
 
-export const  portfolioItems = [
+export const portfolioItems = [
   {
     title: "Controllerfx",
-    description: "A responsive e-commerce website built with Webflow and Shopify.",
+    description:
+      "A responsive e-commerce website built with Webflow and Shopify.",
     image: cntlrfx,
     url: "https://controllerfx.com",
   },
   {
     title: "Toasted Sesame Therapy",
     description: "A custom portfolio website using React and Chakra UI.",
-    image: toastedsesame,
+    image: TST,
     url: "https://toastedsesametherapy.com",
   },
 ];
 
-export const resumeLink = "https://drive.google.com/file/d/18D90KdZPNePThoPtX_c721bNQw9gaaFt/view?usp=sharing"
+export const resumeLink =
+  "https://drive.google.com/file/d/18D90KdZPNePThoPtX_c721bNQw9gaaFt/view?usp=sharing";
 
-export const websiteWindowText = "Websites I've designed and developed"
+export const websiteWindowText = "Websites I've designed and developed";
